2fb71b9d249f7f00231637ab133aa4479415cb5b
kimjuho93 <midnightperfume93@gmail.com>
2019-04-07 22:59:53 +0900

    no message

--- portfolio/work4/js/menu-s.js
+++ portfolio/work4/js/menu-s.js
@@ -1,12 +1,12 @@
 $(function(){
     $('.hamburger-on').click(function(){
-        $('.menu-s').animate({ height: '90px' }, 100); 
+        $('.menu-s').animate({ height: '90px' }, 300); 
         $('.hamburger-off').show();
         $(this).hide();
     });
 
     $('.hamburger-off').click(function(){
-        $('.menu-s').animate({ height: '0px' }, 100); 
+        $('.menu-s').animate({ height: '0px' }, 300); 
         $('.hamburger-on').show();
         $(this).hide();
     });
