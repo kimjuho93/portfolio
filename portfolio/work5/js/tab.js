90fee6c561aec9b0e58f4debf7e66e2a3ea997d1
kimjuho93 <midnightperfume93@gmail.com>
2019-04-25 16:56:22 +0900

    no message

--- portfolio/work5/js/tab.js
+++ portfolio/work5/js/tab.js
@@ -1,23 +1,5 @@
-$(document).ready(function(){
-    let btnTopSelected = $('.btn-top:nth-child(n)');
+$(function(){
     let btnFixedSelected = $('.btn-f:nth-child(n)');
-
-    $(btnTopSelected).click(function(){
-        $('.btn-top').removeClass('btn-top-active');
-        $(this).addClass('btn-top-active');
-        $('.tab').removeClass('tab-w-active');
-
-        let btnTopIndex = $(this).index();
-        for (let i = 0; i < 5; i++) {  
-            if ( btnTopIndex == i) {
-                let tabSelected = $('.tab:nth-child(' + (i + 1) + ')');
-                let tabIndex = $('.tab:nth-child(' + (i + 1) + ')').index();
-                $(tabSelected).addClass('tab-w-active');
-                console.log(tabIndex);
-            }
-        }
-        console.log(btnTopIndex);
-    });
 
     $(btnFixedSelected).click(function(){
         $('.btn-f').removeClass('btn-f-active');
@@ -25,15 +7,60 @@ $(document).ready(function(){
         $('.tab-h').removeClass('tab-h-active');
        
         let btnFixedIndex = $(this).index();
-        for (let i = 0; i < 5; i++) {  
+        for (let i = 0; i < 4; i++) {  
             if ( btnFixedIndex == i) {
                 let tabHomeSelcted = $('.tab-h:nth-child(' + (i + 1) + ')');
-                let tabHomeIndex = $('.tab-h:nth-child(' + (i + 1) + ')').index();
                 $(tabHomeSelcted).addClass('tab-h-active');
-                console.log(tabHomeIndex);
             }
         }
-        console.log(btnFixedIndex);
     });
+
+    let btnRecordSelected = $('.btn-r:nth-child(n)');
+
+    $(btnRecordSelected).click(function(){
+        $('.btn-r').removeClass('btn-r-active');
+        $(this).addClass('btn-r-active');
+        $('.record').removeClass('record-active');
+       
+        let recordIndex = $(this).index();
+        for (let i = 0; i < 4; i++) {  
+            if ( recordIndex == i) {
+                let recordSelcted = $('.record:nth-child(' + (i + 1) + ')');
+                $(recordSelcted).addClass('record-active');
+            }
+        }
+    });
+
+    $(function(){
+        let criteria = $(window).scrollTop();
+        let fixedTab = $('.home-header-fixed');
+        let top = $('.top');
+
+        if(criteria >= 50) {
+            $(fixedTab).addClass('fixed');
+            $(top).css({'margin-top': '45px'});
+        } else {
+            $(fixedTab).removeClass('fixed');
+            $(top).css({'margin-top': '0px'});
+        }
+    });
+    
+    $(window).scroll(function(e){
+        fixedTab();
+    });
+    
+    function fixedTab(){
+        let criteria = $(window).scrollTop();
+        let fixedTab = $('.home-header-fixed');
+        let top = $('.top');
+    
+        if(criteria >= 50) {
+            $(fixedTab).addClass('fixed');
+            $(top).css({'margin-top': '45px'});
+        } else {
+            $(fixedTab).removeClass('fixed');
+            $(top).css({'margin-top': '0px'});
+        }
+    }      
 });
 
